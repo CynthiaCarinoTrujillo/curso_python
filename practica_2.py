b9a0e5fbf976c87e46ca5e9e9739cde507b3fef4
CynthiaCarinoTrujillo <cynthia.carinot@gmail.com>
2026-07-13 22:16:55 +0200

    Practica 2

---
+++ practica_2.py
@@ -0,0 +1,9 @@
+# funciones type() y isinstance()
+#para ver el tipo de una variable se utiliza la funcion type()
+#dentro del parentesis se pone la variable a evaluar
+
+#declaracion de variable
+color_favorito="blanco"
+
+print("El tipo de variable es", type(color_favorito))
+

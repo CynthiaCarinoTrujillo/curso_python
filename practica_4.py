f8cc0fb9d05452c4f7a3ff0ed914641933cb3b27
CynthiaCarinoTrujillo <cynthia.carinot@gmail.com>
2026-07-14 16:07:37 +0200

    Add string slicing examples and enhance comments in practica_4.py

--- practica_4.py
+++ practica_4.py
@@ -33,3 +33,50 @@ print(f"prueba de interpolacion: {name} {peso}")
 
 variable_interpolada=f"yo soy {name}"
 print(variable_interpolada)
+
+num_uno=5
+num_dos=10
+print(f"la suma de {num_uno} y {num_dos} es {num_uno + num_dos}")
+#el valor de las variables num_uno y num_dos se convierte internamente en una cadena durante el proceso de interpolación.
+
+#corte de cadenas y cómo funciona
+
+#cada carácter en una cadena puede ser identificado por su índice (comenzando desde cero)
+#y accedido usando la notación de corchetes
+
+print("Accediendo a una letra dentro de una cadena" , lastname[4]) #imprime una letra de la cadena lastname
+
+#El corte de cadenas te permite extraer una porción de una cadena o trabajar solo con una parte específica de ella. 
+#sintaxis ---> string[start:stop]
+
+print("Corte de cadenas" , lastname[5:8]) #imprime una porcion de la cadena lastname Gonzalez
+
+"""Tener en cuenta que el índice stop no es inclusivo, 
+por lo que [5:8] solo extrae los caracteres desde el índice 5 hasta 
+sin incluir, el carácter del índice 8.
+
+Tambien se puede omitir el indice start o stop y python asumira el valor por defecto"""
+
+print("Omitiendo el indice start" , lastname[:6])
+print("Omitiendo el indice stop", lastname[5:])
+
+# cortar una cadena no modifica la cadena original
+#puedes omitir los índices de start como de stop, lo que extraerá toda la cadena
+
+print(lastname[:]) #imprime toda la cadena lastname
+
+lista_edades=[i for i in range(0,10)]
+#existe un parámetro opcional step, que se usa para especificar el incremento entre cada índice en el slice.
+print("Usando el parámetro step", lastname[0:9:2]) #imprime los caracteres desde el índice 0 hasta el 9, con un incremento de 2
+"""
+sintaxis ---> string[start:stop:step] la division se realiza de la siguiente manera:
+- start: índice inicial desde donde se comenzará a cortar la cadena.
+- stop: índice final hasta donde se cortará la cadena (sin incluir el carácter en este indice)
+- step: especifica el salto a dar mientras recorre la lista. 2 hace que imprimas de 2 en 2
+"""
+print(lista_edades[::2])    #imprime los elementos de la lista desde el índice 0 hasta el final, con un incremento de 2
+print(lista_edades[::3])
+print(lista_edades[::-1]) #si el numero es negativo recorre la lista desde el final, al reves
+
+lista_edades_al_reves = lista_edades[::-1]
+print(lista_edades_al_reves)

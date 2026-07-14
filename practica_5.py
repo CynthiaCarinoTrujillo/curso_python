dd173abf05dc48bd61fa4366160deb9b1ecdb1ce
CynthiaCarinoTrujillo <cynthia.carinot@gmail.com>
2026-07-14 17:25:16 +0200

    Add initial implementation of common string methods in practica_5.py

---
+++ practica_5.py
@@ -0,0 +1,14 @@
+#métodos comunes de cadenas
+"""
+Un método es una función que pertenece a un objeto o clase específicos.
+Los métodos deben ser llamados en el objeto al que pertenecen escribiendo 
+el objeto seguido de un punto y la llamada al método. 
+"""
+variable_cadena="HoLa AmIgOs"
+print(variable_cadena)
+
+#upper(): Devuelve una nueva cadena con todos los caracteres convertidos a mayúsculas.
+print(variable_cadena.upper())
+
+#lower(): Devuelve una nueva cadena con todos los caracteres convertidos a minúsculas.
+print(variable_cadena.lower())

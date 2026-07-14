3b453c70568b2ce20e6e1d0647ca101824c23e88
CynthiaCarinoTrujillo <cynthia.carinot@gmail.com>
2026-07-14 13:11:40 +0200

    Fix age variable value and enhance string concatenation and interpolation examples in practica_4.py

--- practica_4.py
+++ practica_4.py
@@ -8,7 +8,28 @@ print("Prueba de concatenacion " + name + " " + lastname)
 
 #Python requiere que todos los elementos sean cadenas antes de poder concatenarlos.
 #Puedes convertirlos a cadenas 
-edad=54
+edad=32
 datos_persona= name + " " + lastname + " " + str(edad) #convertimos edad a string para poder concatenarla
 
 print(datos_persona)
+
+"""
+También puedes usar el operador de asignación aumentada para la concatenación. Este se 
+representa con un más y un signo igual (+=), y realiza tanto la concatenación como la 
+asignación en un solo paso. 
+"""
+peso=54.3
+datos_persona+= " " + str(peso) #convertimos peso a string para poder concatenarla
+print(datos_persona)
+
+""" El proceso de insertar variables y expresiones en una cadena se llama interpolación de 
+cadenas. Python tiene una categoría de cadena llamada f-strings 
+(abreviatura de literales de cadena formateados)
+Las cadenas f comienzan con f (ya sea en minúsculas o mayúsculas) antes de las comillas, 
+y te permiten insertar variables o expresiones dentro de campos de reemplazo indicados
+por llaves ({}).
+"""
+print(f"prueba de interpolacion: {name} {peso}")
+
+variable_interpolada=f"yo soy {name}"
+print(variable_interpolada)
